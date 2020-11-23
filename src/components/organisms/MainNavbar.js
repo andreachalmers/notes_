1014906c3af6b603d37ed33e67360f6c2ecd00af
Andrea Chalmers <a.chalmers@douglas.de>
2020-11-23 23:04:28 +0100

    add icons

--- src/components/organisms/MainNavbar.js
+++ src/components/organisms/MainNavbar.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import ListItem from "../atoms/ListItem";
+import {HiOutlineTrash} from "react-icons/hi"
+import {FcTodoList} from "react-icons/fc"
+import {BiNotepad} from "react-icons/bi"
 import React from 'react';
 
 const Nav = styled.nav`
@@ -12,22 +15,38 @@ const Nav = styled.nav`
 	
 	.nav-list {
 		margin-top: 68px;
+		padding-left: 0;
 	}
 `;
 
 const Li = styled.li`
 	list-style: none;
-	padding: 8px;
+	padding: 8px 8px 8px 40px;
 	width: 100%;
+	background-color: ${props => props.active ? `var(--color2)`: `transparent`};
+	color: ${props => props.active ? `var(--color5)`: `var(--color3)`};
+	font-weight: bold;
+	display: flex;
+	align-items: center;
+	
+	svg {
+		stroke:  ${props => props.active ? `var(--color1)`: `var(--color3)`};
+		width: 1.5em;
+		height: 1.5em;
+		
+		&.todo g {
+			fill: ${props => props.active ? `var(--color1)`: `var(--color3)`};
+		}
+	}
 `;
 
 const MainNavbar = () => {
 	return (
 		<Nav>
 			<ul className="nav-list">
-				<Li>Notes</Li>
-				<Li>Todo</Li>
-				<Li>Trash</Li>
+				<Li active><BiNotepad className="mr-8"/>Notes</Li>
+				<Li><FcTodoList className="mr-8 todo"/>Todo</Li>
+				<Li><HiOutlineTrash className="mr-8"/>Trash</Li>
 			</ul>
 		</Nav>
 	);
--- src/components/organisms/Sidebar2.js
+++ src/components/organisms/Sidebar2.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BsPencilSquare } from 'react-icons/bs';
 import {Search, List, Icon} from "semantic-ui-react";
 import React from "react";
 import CenterAlign from "../helpers/CenterAlign";
@@ -30,7 +31,7 @@ const SidebarContainer = styled.aside`
 			border-radius: 2px;
 			background: var(--color1);
 			color: white;
-		
+			outline: none;
 		}
 		
 		i {
@@ -49,12 +50,18 @@ const SidebarHeader = styled.header`
 	display: flex;
 	padding: 16px;
 	justify-content: space-around;
-	align-items: centers;
+	align-items: center;
 	border-bottom: 1px solid var(--color4);
 	position: sticky;
 	top: 0;
 	z-index: 400;
 	background-color: var(--color1);
+	
+	.sidebar__svg {
+		fill: var(--color3);
+		width: 1.5em;
+		height: 1.5em;
+	}
 `;
 //@todo: box shadow when scrolling
 
@@ -63,7 +70,7 @@ const Sidebar2 = ({addNote, children}) => {
 		<SidebarContainer>
 			<SidebarHeader>
 				<Search placeholder="Search notes" fluid size={"small"} />
-				<Icon name="write square"  onClick={addNote} color={"red"} size={"big"} style={{position: 'relative', top: '4px'}}/>
+				<BsPencilSquare onClick={addNote} className="sidebar__svg btn"/>
 			</SidebarHeader>
 			{children}
 		</SidebarContainer>
